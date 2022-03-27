315208d758875d1acbbdde7982d3ee4615ca1b2e
RyotaNishio <78110301+Sena-7v@users.noreply.github.com>
2022-03-27 10:47:23 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import {useState} from 'react'
 
-const App = () => {
-    const [count, setCount] = useState(0)
+const App = props => {
+    const initialStates = {
+        name: 'sample',
+        price: 1000
+    }
 
-    const increment = () => setCount(count+1)
-    const decrement = () => setCount(count-1)
-    const increment2 = () => setCount(previousCount => previousCount+1)
-    const decrement2 = () => setCount(previousCount => previousCount-1)
-    const reset = () => setCount(0)
-    const double = () => setCount(count*2)
-    const divide = () => setCount(count %  3 === 0 ? count / 3 : count)
+    const reset = () => {
+        setPrice(initialStates.price)
+        setName(initialStates.name)
+    }
 
-
-
-
-
+    const [name, setName] = useState(initialStates.name)
+    const [price, setPrice] = useState(initialStates.price)
     return (
-        <>
-            <div>count: {count}</div>
-            <div>
-                <button onClick={increment}>+1</button>
-                <button onClick={decrement}>-1</button>
-            </div>
-            <div>
-                <button onClick={increment2}>+1</button>
-                <button onClick={decrement2}>-1</button>
-            </div>
-            <div>
-                <button onClick={reset}>reset</button>
-            </div>
-            <div>
-                <button onClick={double}>double</button>
-            </div>
-            <div>
-                <button onClick={divide}>divide</button>
-            </div>
-        </>
+    <>
+        <p>現在の{name}は、{price}円です。</p>
+        <button onClick={() => setPrice(price + 1)}>+1</button>
+        <button onClick={() => setPrice(price - 1)}>-1</button>
+        <button onClick={reset}>reset</button>
+        <input value={name} onChange={e => setName(e.target.value)}/>
+    </>
     );
 }
 
